ff7b337d22a6e3f2ee7301157980c56896b27ab8
Liutx <15666882775@163.com>
2020-12-16 12:00:41 +0800

    RedisConfig

--- src/test/java/org/javaboy/redis/RedisApplicationTests.java
+++ src/test/java/org/javaboy/redis/RedisApplicationTests.java
@@ -1,13 +1,21 @@
 package org.javaboy.redis;
 
+import org.javaboy.redis.config.RedisService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class RedisApplicationTests {
 
+    @Autowired
+    private RedisService redisUtil;
+
+
     @Test
     void contextLoads() {
+        Object o = new Object();
+        redisUtil.setCacheObject("111",o);
     }
 
 }
